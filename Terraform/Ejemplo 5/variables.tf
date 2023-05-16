990596f8fa5de58fc1e76b3fe122d5b575dd8731
amiguel215 <90587057+amiguel215@users.noreply.github.com>
2023-05-16 17:23:32 -0500

    Add files via upload

---
+++ Terraform/Ejemplo 5/variables.tf
@@ -0,0 +1,25 @@
+variable "resource_group_name" {
+  type = string
+
+}
+variable "location" {
+  type    = string
+  default = "West Europe"
+
+}
+variable "service_plan_name" {
+  type = string
+}
+variable "sku_name" {
+  type = string
+}
+variable "os_type" {
+  type = string
+
+}
+variable "windows_app_services" {
+  type = map(any)
+}
+variable "environment" {
+  type = string
+}
